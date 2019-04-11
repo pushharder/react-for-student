205914b4df58a1e2feea1c4748d8aaee291d3aea
alex.milovanov.mb@gmail.com <pushharder>
2019-04-11 15:49:26 +0300

    prettier and linter and husky

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,15 +3,13 @@ import * as React from 'react';
 import IPage from './app.interfaces';
 
 class App extends React.Component<IPage, {}> {
-  render() {
-    return (<div>
-      <h1>Welcome to React with Typescript</h1>
-      <p>The color of this page is: {this.props.color}</p>
-    </div>
+  public render() {
+    return (
+      <div>
+        <h1>Welcome to React with Typescript</h1>
+        <p>The color of this page is: {this.props.color}</p>
+      </div>
     );
-  }
-  someMethod = () => {
-  
   }
 }
 
--- src/components/app.interfaces.tsx
+++ src/components/app.interfaces.tsx
@@ -1,3 +1,3 @@
 export default interface IPage {
-    color: string;
+  color: string;
 }
